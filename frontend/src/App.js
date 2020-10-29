a68e1b2cbdaf3d1b2afc634c7f4be351964c2a11
koojaa <sualyer010@snu.ac.kr>
2020-10-29 17:26:26 +0900

    Add husky & jsconfig

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App = () => {
-  return (
-    <div>
-      team 18    
-    </div>
-  );
-};
+  return <div>team 18</div>
+}
 
-export default App;
+export default App
