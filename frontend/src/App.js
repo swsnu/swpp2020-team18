9ea3c3cfdf9ad5fdb16d44efbfde393405862363
Jaewoo Maeng <maeng9584@gmail.com>
2020-11-09 06:16:31 +0900

    Landing page middle commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,65 @@ import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import Landing from './containers/Landing'
+import Landing1 from './containers/Landing1'
 import PropTypes from 'prop-types'
 import SignIn from './components/accounts/SignIn'
 import SignUp from './components/accounts/SignUp'
-import AccountBox from './containers/accounts/AccountBox'
+// import AccountBox from './containers/accounts/AccountBox'
+import './App.css'
+import { ThemeProvider } from '@material-ui/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ff784e',
+      main: '#00bcd4',
+      dark: '#b23c17',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#dd33fa',
+      main: '#d500f9',
+      dark: '#9500ae',
+      contrastText: '#fff',
+    },
+  },
+})
 
 function App(props) {
   return (
     <ConnectedRouter history={props.history}>
-      <div className='App'>
-        <Switch>
-          <Route
-            path='/terminator'
-            exact
-            render={() => (
-              <>
-                <AccountBox />
-                <Landing />
-              </>
-            )}
-          />
-          <Route path='/signin' exact render={() => <SignIn />} />
-          <Route path='/signup' exact render={() => <SignUp />} />
-          <Redirect exact from='/' to='terminator' />
-          <Route render={() => <h1>Not Found</h1>} />
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className='App'>
+          <Switch>
+            <Route
+              path='/terminator'
+              exact
+              render={() => (
+                <>
+                  {/* <AccountBox /> */}
+                  <Landing />
+                </>
+              )}
+            />
+            <Route
+              path='/terminator/article'
+              exact
+              render={() => (
+                <>
+                  {/* <AccountBox /> */}
+                  <Landing1 />
+                </>
+              )}
+            />
+            <Route path='/signin' exact render={() => <SignIn />} />
+            <Route path='/signup' exact render={() => <SignUp />} />
+            <Redirect exact from='/' to='terminator' />
+            <Route render={() => <h1>Not Found</h1>} />
+          </Switch>
+        </div>
+      </ThemeProvider>
     </ConnectedRouter>
   )
 }
--- frontend/src/containers/Landing.js
+++ frontend/src/containers/Landing.js
@@ -1,10 +1,143 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
 // import { connect } from 'react-redux';
+import articleImage from '../images/article.png'
+import wordlistImage from '../images/wordlist.svg'
+import testImage from '../images/exam.svg'
+import statisticsImage from '../images/bar-chart.svg'
+import '../App.css'
+import './Landing.css'
+import logoImage from '../images/logo.png'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Link from '@material-ui/core/Link'
 
-class Landing extends Component {
-  render() {
-    return <div>Landing Page</div>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  logoutButton: {
+    Right: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  logo: {
+    marginRight: theme.spacing(2),
+    maxWidth: 160,
+  },
+  card: {
+    padding: 10,
+  },
+  cardContent: {
+    height: 10,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cover: {
+    width: 151,
+  },
+}))
+
+function Landing(props) {
+  const classes = useStyles()
+
+  if (props.user) {
+    return <Redirect to='/signin' />
   }
+
+  return (
+    <Fragment className='frame flex-center'>
+      <div className={classes.root}>
+        <AppBar position='static'>
+          <Toolbar>
+            <img src={logoImage} alt='terminator' className={classes.logo} />
+            <Typography variant='h6' className={classes.title}></Typography>
+            <Button className={classes.logoutButton} color='inherit'>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+
+      <div className='container'>
+        <Link
+          className='item article'
+          underline='none'
+          component={RouterLink}
+          to='/terminator/article'
+        >
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                alt='Article'
+                // height='100%'
+                image={articleImage}
+                title='Article'
+                // inline-block='false'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  Article Recommendation
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  Learn words from context through recommended articles! Articles
+                  will be recommended based on your word proficiency.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Link>
+        <Card className='item wordlist'>
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component='h5' variant='h5'>
+                Live From Space
+              </Typography>
+              <Typography variant='subtitle1' color='textSecondary'>
+                Mac Miller
+              </Typography>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image={wordlistImage}
+            title='Live from space album cover'
+          />
+        </Card>
+        <button className='item test'>
+          <img src={testImage} width='30%' alt='test' />
+        </button>
+        <button className='item stats'>
+          <img src={statisticsImage} width='30%' alt='stats' />
+        </button>
+      </div>
+    </Fragment>
+  )
 }
 
-export default Landing
+const mapStateToProps = (state) => ({
+  user: state.accounts.user,
+})
+
+Landing.propTypes = {
+  user: PropTypes.object,
+}
+
+export default connect(mapStateToProps, null)(Landing)
