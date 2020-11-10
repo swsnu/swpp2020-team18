6d0faf325c407fbeb99eb28b19d48797fa4ca2b0
Jaewoo Maeng <maeng9584@gmail.com>
2020-11-11 03:46:49 +0900

    Test pages with fake data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import { createMuiTheme } from '@material-ui/core/styles'
 import Article from 'containers/article/Article'
 import Wordlist from 'containers/wordlist/Wordlist'
 import Statistics from 'containers/statistics/Statistics'
-import Test from 'containers/test/Test'
+import ReviewTest from 'containers/test/ReviewTest'
 import { connect } from 'react-redux'
 import * as accounts from './ducks/accounts'
 import ArticleView from './containers/article/ArticleView'
@@ -45,7 +45,7 @@ function App(props) {
         <div className='App'>
           <Switch>
             <Route
-              path='/terminator'
+              path='/'
               exact
               render={() => (
                 <>
@@ -54,7 +54,7 @@ function App(props) {
               )}
             />
             <Route
-              path='/terminator/article'
+              path='/article'
               exact
               render={() => (
                 <>
@@ -63,7 +63,7 @@ function App(props) {
               )}
             />
             <Route
-              path='/terminator/wordlist'
+              path='/wordlist'
               exact
               render={() => (
                 <>
@@ -72,16 +72,16 @@ function App(props) {
               )}
             />
             <Route
-              path='/terminator/test'
+              path='/test'
               exact
               render={() => (
                 <>
-                  <Test />
+                  <ReviewTest />
                 </>
               )}
             />
             <Route
-              path='/terminator/statistics'
+              path='/statistics'
               exact
               render={() => (
                 <>
