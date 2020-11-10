e8bf0147290f96a080f798a89d7404b80c24f482
chanseck <chanseck@naver.com>
2020-11-10 16:06:31 +0900

    Add annotation in models.py

--- backend/wordtest/models.py
+++ backend/wordtest/models.py
@@ -4,9 +4,24 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 User = get_user_model()
-from wordlist.models import Phrase, Wordlist, AddPhrase
+from wordlist.models import Word, Phrase, Wordlist, WordlistPhrase
 
 class Question(models.Model):
+    """
+    A class to represent questions in test
+    :param taker: User who take test
+    :type taker: class:`django.db.models.ForeignKey`
+    :param question: a selected phrase
+    :type question: class:`django.db.models.ForeignKey`
+    :param number: the question number
+    :type number: class: django.db.models.IntegerField
+    :param answer1, 2, 3, 4: question choices
+    :type answer1, 2, 3, 4: class:`django.db.models.TextField`
+    :param correct_answer: the number of correct answer
+    :type correct_answer: class: django.db.models.IntegerField
+    :param completed: mean test is completed or not
+    :type completed: class: django.db.models.BooleanField
+    """
     taker = models.ForeignKey(
         User,
         on_delete=models.CASCADE
@@ -22,9 +37,25 @@ class Question(models.Model):
     answer4 = models.CharField(max_length=100)
     correct_answers = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.question
 
-# save test result
 class TestResult(models.Model):
+    """
+    A class to save test result
+    there are two types of test, 'AR' for article and 'WL' for word list.
+    :param test_date: date when test is taken
+    :type test_date: class:`django.db.models.DateTimeField`
+    :param test_type: two types of test
+    :type test_type: class:`django.db.models.CharField`
+    :param question_count: the count of questions
+    :type question_count: class:`django.db.models.IntegerField`
+    :param solution_count: the count of corrected
+    :type solution_count: class:`django.db.models.IntegerField`
+    :param score: score of the test
+    :type score: class:`django.db.models.DecimalField`
+    """
     TEST_TYPE = [
         ('AR','article'),
         ('WL','wordlist'),
@@ -42,6 +73,13 @@ class TestResult(models.Model):
 # one user should have one TestHistory model
 # create TestHistory when user created
 class TestHistory(TestResult):
+    """
+    A class to save the user's test history
+    :param user_id: user id who own this model
+    :type user_id: class:`django.db.models.ForeignKey`
+    :param test_id: test id which mean order of the test
+    :type test_id: class:`django.db.models.IntegerField`
+    """
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     test_id = models.IntegerField(primary_key=True,blank=False)
 
