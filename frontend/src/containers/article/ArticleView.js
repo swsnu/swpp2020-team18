7e62fa8c26674c7a5cad7de5dffa8e8f6b845683
Seungwoo Jo <sjosjo1204@gmail.com>
2020-11-28 09:16:22 +0900

    Remove ArticleBox.js

--- frontend/src/containers/article/ArticleView.js
+++ frontend/src/containers/article/ArticleView.js
@@ -26,22 +26,8 @@ const useStyles = makeStyles((theme) => ({
     margin: 0,
     padding: '5vh',
   },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
   stepper: {
     backgroundColor: '#fafafa',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
