252e5b4abec38f5cda708e1a4b8fa75c29fcf574
Seungwoo Jo <sjosjo1204@gmail.com>
2020-12-15 23:42:29 +0900

    Fix FE test bug

--- frontend/src/components/accounts/SignIn.test.js
+++ frontend/src/components/accounts/SignIn.test.js
@@ -1,46 +1,41 @@
-// import React from 'react';
+import React from 'react'
 // import { shallow, mount } from 'enzyme';
-// import SignIn from './SignIn';
-// import { getMockStore } from '../../test-utils/mocks';
-// import { history } from '../../test-utils/mocks';
+import SignIn from './SignIn'
+import { getMockStore } from '../../test-utils/mocks'
+import { history } from '../../test-utils/mocks'
 // import * as actionCreators from '../../ducks/accounts';
-// import { Provider } from 'react-redux';
-// import { connectRouter, ConnectedRouter } from 'connected-react-router';
-// import { Route, Redirect, Switch } from 'react-router-dom';
-// import { createShallow } from '@material-ui/core/test-utils';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router-dom'
+import { createMount } from '@material-ui/core/test-utils'
 
-// const stubInitialState = {
-//   user: null
-// };
+const stubInitialState = {
+  user: null,
+}
 
-// const mockStore = getMockStore(stubInitialState);
+const mockStore = getMockStore(stubInitialState)
 
 describe('<SignIn />', () => {
-  // let signIn;
-  // let mount;
+  let signIn
+  let mount
 
-  // beforeEach(() => {
-  //   mount = createShallow();
-  //   signIn = (
-  //     <Provider store={mockStore}>
-  //       <ConnectedRouter history={history}>
-  //       <Switch>
-  //         <Route path='/' exact render={() => <SignIn />} />
-  //       </Switch>
-  //       </ConnectedRouter>
-  //     </Provider>
-  //   );
-  //   // spyGetArticles = jest.spyOn(actionCreators, 'getArticles')
-  //   //   .mockImplementation(() => { return dispatch => {}; });
-  // })
+  beforeEach(() => {
+    mount = createMount()
+    signIn = (
+      <Provider store={mockStore}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route path='/' exact render={() => <SignIn />} />
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    )
+  })
 
-  // it('should render without errors', () => {
-  //   const component = mount(signIn);
-  //   console.log(component.debug())
-  //   const wrapper = component.find('.fake');
-  //   expect(wrapper.length).toBe(1);
-  // });
-  it('fake test', () => {
-    console.log('TODO: Please remove this test.')
+  it('should render without errors', () => {
+    const component = mount(signIn)
+    // console.log(component.debug())
+    const wrapper = component.find('SignIn')
+    expect(wrapper.length).toBe(1)
   })
 })
