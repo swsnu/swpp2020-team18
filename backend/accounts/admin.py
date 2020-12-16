ed3965ec98717e3d152ab8a6bd71e94c0fdf8870
Seungwoo Jo <sjosjo1204@gmail.com>
2020-12-16 17:01:01 +0900

    Add DailyRecord to admin site

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -3,6 +3,7 @@ Register accounts models to admin site
 """
 
 from django.contrib import admin
-from .models import User
+from .models import User, DailyRecord
 
 admin.site.register(User)
+admin.site.register(DailyRecord)
