cc5abe6cfad7ad6d6f8707447206ad9acdcff90f
Seungwoo Jo <sjosjo1204@gmail.com>
2020-12-17 11:46:58 +0900

    Add fillMatrix

--- backend/article/recommend.py
+++ backend/article/recommend.py
@@ -40,6 +40,7 @@ def makeMatrix():
         
     return matrix
 
+
 def centeredCosineSimilarity(v1, v2):
     v1 = v1 - np.nanmean(v1)
     v2 = v2 - np.nanmean(v2)
@@ -50,9 +51,51 @@ def centeredCosineSimilarity(v1, v2):
 
     return cos_sim
 
+
 def fillMatrix(matrix):
     """
     Fill out matrix
     with collaborative filtering method
     """
-    
+    filled_matrix = np.empty((0, len(words)), dtype=np.float32)
+    similarity_matrix = np.zeros((len(users), len(users)), dtype=np.float32)
+    DEFAULT_VALUE = 5
+    TOP_N = 3
+
+    # Calculate similarity matrix
+    for i in range(len(users)):
+        for j in range(i+1):
+            ccs = centeredCosineSimilarity(matrix[i], matrix[j])
+            similarity_matrix[i][j] = ccs
+            similarity_matrix[j][i] = ccs
+
+    for user_idx, user in enumerate(users):
+        user_word_confidence_row_list = [np.nan]*len(words)
+        for word_idx, word in enumerate(word):
+            if not np.isnan(matrix[user_idx][word_idx]):
+                expected_confidence = matrix[user_idx][word_idx]
+            else:
+                has_word_confidence_idx_list = []
+                confidence_list = []
+                for i in range(len(users)):
+                    if not np.isnan(matrix[i][word_idx]):
+                        has_word_confidence_idx_list.append(i)
+                        confidence_list.append(matrix[i][word_idx])
+                similarity_list = list(map(lambda idx: similarity_matrix[user_idx][idx], has_word_confidence_idx_list))
+                
+                if len(has_word_confidence_idx_list) == 0:
+                    expected_confidence = DEFAULT_VALUE
+                else:
+                    zipped = list(zip(similarity_list, confidence_list, has_word_confidence_idx_list))
+                    zipped.sort()
+                    zipped = zipped[:TOP_N]
+                    sum_similarity = 0
+                    sum_weight = 0
+                    for sim, conf, i in zipped:
+                       sum_weight += conf * sim
+                       sum_similarity += sim
+                    expected_confidence = sum_weight / sum_similarity 
+
+            filled_matrix[user_idx][word_idx] = expected_confidence
+                    
+    return filled_matrix
