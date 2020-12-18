c89e3aa567eac9f438ac46fccf1ff3d4f843e464
qwerty98765 <maeng9584@naver.com>
2020-12-18 17:15:13 +0900

    test code for wordtest

--- backend/wordtest/models.py
+++ backend/wordtest/models.py
@@ -39,10 +39,6 @@ class History(models.Model):
         _ = (sender, kwargs,)
         instance.history.save()
 
-# TODO
-# 찬석님 코드 활용 방법 강구할 것
-# below are codes from chanseck
-
 class HistoryWord(models.Model):
     """
     A class to manage relationship between history and word.
@@ -58,90 +54,3 @@ class HistoryWord(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     confidence = models.IntegerField(default=1)
-
-class Question(models.Model):
-    """
-    A class to represent questions in test
-    :param taker: User who take test
-    :type taker: class:`django.db.models.ForeignKey`
-    :param question: a selected phrase
-    :type question: class:`django.db.models.ForeignKey`
-    :param number: the question number
-    :type number: class: django.db.models.IntegerField
-    :param answer1, 2, 3, 4: question choices
-    :type answer1, 2, 3, 4: class:`django.db.models.TextField`
-    :param correct_answer: the number of correct answer
-    :type correct_answer: class: django.db.models.IntegerField
-    :param completed: mean test is completed or not
-    :type completed: class: django.db.models.BooleanField
-    """
-
-    taker = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Phrase, on_delete=models.CASCADE)
-    number = models.IntegerField(default=0)
-    answer1 = models.CharField(max_length=100)
-    answer2 = models.CharField(max_length=100)
-    answer3 = models.CharField(max_length=100)
-    answer4 = models.CharField(max_length=100)
-    correct_answers = models.IntegerField(default=0)
-    completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.question
-
-
-class TestResult(models.Model):
-    """
-    A class to save test result
-    there are two types of test, 'AR' for article and 'WL' for word list.
-    :param test_date: date when test is taken
-    :type test_date: class:`django.db.models.DateTimeField`
-    :param test_type: two types of test
-    :type test_type: class:`django.db.models.CharField`
-    :param question_count: the count of questions
-    :type question_count: class:`django.db.models.IntegerField`
-    :param solution_count: the count of corrected
-    :type solution_count: class:`django.db.models.IntegerField`
-    :param score: score of the test
-    :type score: class:`django.db.models.DecimalField`
-    """
-
-    TEST_TYPE = [
-        ("AR", "article"),
-        ("WL", "wordlist"),
-    ]
-
-    test_date = models.DateTimeField()
-    test_type = models.CharField(max_length=10, choices=TEST_TYPE)
-    question_count = models.IntegerField(default=20)
-    solution_count = models.IntegerField(null=True)
-    score = models.DecimalField(max_digits=4, decimal_places=2)
-
-    def __str__(self):
-        return "test score: %d" % score
-
-
-# one user should have one TestHistory model
-# create TestHistory when user created
-# class TestHistory(TestResult):
-#     """
-#     A class to save the user's test history
-#     :param user_id: user id who own this model
-#     :type user_id: class:`django.db.models.ForeignKey`
-#     :param test_id: test id which mean order of the test
-#     :type test_id: class:`django.db.models.IntegerField`
-#     """
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-#     test_id = models.IntegerField(primary_key=True,blank=True)
-
-#     def __str__(self):
-#         return "%s's test result history" % self.user.username
-
-# @receiver(post_save, sender=User)
-# def create_test_history(sender, instance, created, **kwargs):
-#     if created:
-#         TestHistory.objects.create(user=instance)
-
-# @receiver(post_save, sender=User)
-# def save_test_history(sender, instance, **kwargs):
-#     instance.testhistory.save()
--- backend/wordtest/views.py
+++ backend/wordtest/views.py
@@ -20,7 +20,6 @@ def history(request):
         if request.method == "POST":
             req_data = json.loads(request.body.decode())
             words = req_data["words"]
-            print(req_data)
             user_history = request.user.history
             for word_data in words:
                 try:
@@ -147,81 +146,3 @@ def level_test(request):
 # TODO
 # 찬석님 코드 활용 방법 강구할 것
 # below are codes from chanseck
-
-def get_random_choice():
-    """
-    Get random korean meaning in Word model as test choices
-
-    :returns: string of random meaning
-    :rtype: string
-    """
-    max_id = Word.objects.all().aggregate(max_id=Max("id"))["max_id"]
-    while True:
-        pk = random.randint(1, max_id)
-        choice = Word.objects.filter(pk=pk).first().korean_meaning
-        if choice:
-            return choice
-
-
-def wordtest(request):
-    """
-    Get a number of phrase from user's word list to make test
-    need to select proper number of phrases.
-    In **GET**: Get a number of phrases and choices
-    In **POST**: Post answer and test result
-
-    :param request: a HTTP request
-    :type request: HttpRequest
-
-    :returns: a JSON response with questions
-    :rtype: JsonResponse
-    """
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            taker = User.objects.get(user=request.user)
-            taker_wordlist = WordlistPhrase.objects.filter(user=taker)
-            question_number = 1
-            question_list = []
-
-            if taker_wordlist.count() < 20:
-                for phrase in taker_wordlist.all().values():
-                    question = {
-                        "number": question_number,
-                        "phrase": phrase["phrase_content"],
-                        "word": phrase["phrase_word"],
-                        "correct": phrase["korean_meaning"],
-                        "wrong1": get_random_choice(),
-                        "wrong2": get_random_choice(),
-                        "wrong3": get_random_choice(),
-                    }
-                    question_list.append(question)
-                    question_number += 1
-            else:
-                for confidence in range(1, 5):
-                    for phrase in (
-                        taker_wordlist.filter(confidence=confidence).all().values()
-                    ):
-                        if question_list.count() >= 20:
-                            break
-                        question = {
-                            "number": question_number,
-                            "phrase": phrase["phrase_content"],
-                            "word": phrase["phrase_word"],
-                            "correct": phrase["korean_meaning"],
-                            "wrong1": get_random_choice(),
-                            "wrong2": get_random_choice(),
-                            "wrong3": get_random_choice(),
-                        }
-                        question_list.append(question)
-                        question_number += 1
-
-            return JsonResponse(question_list, safe=False)
-        else:
-            return HttpResponse(status=401)
-
-    elif request.method == "POST":
-        # TODO
-        return JsonResponse()
-
-    else:
-        return HttpResponseNotAllowed(["GET", "POST"])
